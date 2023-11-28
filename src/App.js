15cfee41eb3395aa68f8c24290bd83dfdebbf533
BREACH1247 <sinhaditya50@gmail.com>
2023-11-28 12:39:11 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {refer} from './components/Reference/ref'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div><h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <refer/>
+  </div>
+
   );
 }
 
