3b526ea3ed25eeeeba6fd74720fda5de3a4940f6
muKaustav <mu.kaustav@gmail.com>
2023-11-28 12:56:24 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import {refer} from './components/Reference/ref'
+import './App.css'
+import Reference from './components/Reference/Reference.jsx'
+
 function App() {
   return (
     <div><h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <refer/>
-  </div>
+      Hello world!
+    </h1>
+      <Reference />
+    </div>
 
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Reference/Reference.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Reference = () => {
+	return (
+		<div className="bg-black">
+			<p>Hello World</p>
+		</div>
+	)
+}
+
+export default Reference
