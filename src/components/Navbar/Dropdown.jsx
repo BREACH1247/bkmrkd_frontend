4e507d2e7f649893184cd8a5dac428420cac47a4
adicr7fan <adikshan11@gmail.com>
2023-12-07 15:08:04 +0530

    login page

--- src/components/Navbar/Dropdown.jsx
+++ src/components/Navbar/Dropdown.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
 
-const Dropdown = ({ isSignedIn, setIsSignedIn, name }) => (
+const Dropdown = ({ setIsSignedIn}) => {
+	
+	const [cookies, removeCookie] = useCookies(['token']);
+	const isSignedIn = !!cookies.token;
+	const name = isSignedIn ? 'User' : '';
+	const handleSignOut = async () => {
+		try {
+		  const response = await axios.post('http://43.205.231.10:5000/api/users/logout');
+		  console.log('Sign out successful:', response.data);
+	  
+		  removeCookie('token', { path: '/' }); // remove the token cookie
+		  setIsSignedIn(false); // set isSignedIn to false
+	  
+		  window.location.reload(); // refresh the page
+		} catch (error) {
+		  console.error('Sign out failed:', error);
+		}
+	  };
+	  return(
 	<div
 		className="origin-top-left absolute right-0 mt-2 w-56 mr-3 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 border border-black z-50 inline-block text-left "
 		style={{ backgroundColor: '#ece4c9' }}
@@ -32,7 +52,7 @@ const Dropdown = ({ isSignedIn, setIsSignedIn, name }) => (
 			</a>
 			{!isSignedIn && (
 				<a
-					href="#"
+					href="/account"
 					onClick={() => {
 						setIsSignedIn(true)
 					}}
@@ -60,7 +80,7 @@ const Dropdown = ({ isSignedIn, setIsSignedIn, name }) => (
 					</a>
 					<a
 						href="#"
-						onClick={() => setIsSignedIn(false)}
+						onClick= {handleSignOut}
 						className="block px-4 py-2 text-sm text-gray-800 hover:bg-gray-100"
 						role="menuitem"
 					>
@@ -71,5 +91,6 @@ const Dropdown = ({ isSignedIn, setIsSignedIn, name }) => (
 		</div>
 	</div>
 )
+};
 
 export default Dropdown
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,43 +1,143 @@
-// Account.jsx
-
-import React, { useState } from 'react';
-import Navbar from '../components/Navbar/Navbar';
-
+import React, { useState } from "react";
+import Navbar from "../components/Navbar/Navbar";
+import axios from "axios";
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 const Account = () => {
+  const navigate = useNavigate();
+  const [cookies, setCookie] = useCookies(['token']);
   const [showSignUp, setShowSignUp] = useState(false);
   const [showSignIn, setShowSignIn] = useState(true);
+  const cardStyle = {
+    borderRadius: "24px",
+    background: "linear-gradient(145deg, #d4cdb5, #fdf4d7)",
+    boxShadow: "11px 11px 29px #c2bba5,-11px -11px 29px #ffffed",
+  };
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://43.205.231.10:5000/api/users/signup', {
+        name: e.target.elements.name.value,
+        email: e.target.elements.email.value,
+        password: e.target.elements.password.value,
+      });
+      console.log('Sign up successful:', response.data);
+
+      setCookie('token', response.data.data.token, { path: '/' }); 
+      if (response.data.status === 'success') {
+        navigate('/');
+    }
+    } catch (error) {
+      console.error('Sign up failed:', error);
+      alert(error.response.data.message);
+    }
+    
+  };
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://43.205.231.10:5000/api/users/login",
+        {
+          email: e.target.elements.email.value,
+          password: e.target.elements.password.value,
+        }
+      );
+      console.log("Sign in successful:", response.data);
+
+      setCookie("token", response.data.data.token, { path: "/" });
+      if (response.data.status === 'success') {
+        navigate('/'); 
+    }
+    } catch (error) {
+      console.error("Sign in failed:", error);
+      alert(error.response.data.message);
+    }
+   
+  };
 
   return (
     <div className="container max-w-7xl mx-auto">
       <Navbar />
       <div className="flex items-center justify-center min-h-screen">
-      <div className="p-8 shadow-lg border rounded-lg border-gray-400 w-96 ">
-        {showSignUp && (
-          <div>
-            
-            <h1 className="text-center mb-4">Sign Up</h1>
-            <form >
-              <input className="block w-full mb-4 p-2 border rounded-lg" type="text" placeholder="Username" />
-              <input className="block w-full mb-4 p-2 border rounded-lg" type="email" placeholder="Email" />
-              <input className="block w-full mb-4 p-2 border rounded-lg" type="password" placeholder="Password" />
-              <button className="block w-full mb-4 p-2 bg-yellow-400 hover:bg-yellow-500 text-black rounded-full" type="submit">Sign Up</button>
-            </form>
-            <button className="block w-full p-2 hover:bg-gray-300 text-blue-500 rounded-full" onClick={() => { setShowSignUp(false); setShowSignIn(true); }}>Sign In Instead</button>
-          </div>
-        )}
-        {showSignIn && (
-          <div>
-            <h1 className="text-center mb-4">Sign In</h1>
-          
-            <form>
-              <input className="block w-full mb-4 p-2 border rounded-lg" type="email" placeholder="Email" />
-              <input className="block w-full mb-4 p-2 border rounded-lg" type="password" placeholder="Password" />
-              <button className="block w-full mb-4 p-2 bg-yellow-400 hover:bg-yellow-500 text-black rounded-full" type="submit">Sign In</button>
-            </form>
-            <button className="block w-full p-2  hover:bg-gray-300 text-blue-500 rounded-full" onClick={() => { setShowSignIn(false); setShowSignUp(true); }}>Sign Up</button>
-          </div>
-        )}
+        <div className="p-8 w-96 " style={cardStyle}>
+          {showSignUp && (
+            <div>
+              <h1 className="text-center mb-4">Sign Up</h1>
+              <form onSubmit={handleSignUp}>
+                <input
+                  className="block w-full mb-4 p-2 border rounded-lg"
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                />
+                <input
+                  className="block w-full mb-4 p-2 border rounded-lg"
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                />
+                <input
+                  className="block w-full mb-4 p-2 border rounded-lg"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                />
+                <button
+                  className="block w-full mb-4 p-2 bg-yellow-400 hover:bg-yellow-500 text-black rounded-full"
+                  type="submit"
+                >
+                  Sign Up
+                </button>
+              </form>
+              <button
+                className="block w-full p-2 hover:bg-gray-300 text-blue-500 rounded-full"
+                onClick={() => {
+                  setShowSignUp(false);
+                  setShowSignIn(true);
+                }}
+              >
+                Sign In Instead
+              </button>
+            </div>
+          )}
+          {showSignIn && (
+            <div>
+              <h1 className="text-center mb-4">Sign In</h1>
+              <form onSubmit={handleSignIn}>
+                <input
+                  className="block w-full mb-4 p-2 border rounded-lg"
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                />
+                <input
+                  className="block w-full mb-4 p-2 border rounded-lg"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                />
+                <button
+                  className="block w-full mb-4 p-2 bg-yellow-400 hover:bg-yellow-500 text-black rounded-full"
+                  type="submit"
+                >
+                  Sign In
+                </button>
+              </form>
+              <button
+                className="block w-full p-2  hover:bg-gray-300 text-blue-500 rounded-full"
+                onClick={() => {
+                  setShowSignIn(false);
+                  setShowSignUp(true);
+                }}
+              >
+                Sign Up
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
