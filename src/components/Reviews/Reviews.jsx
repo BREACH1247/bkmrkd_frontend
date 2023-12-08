528ccdfee79d26fdf0dff35fa4f1345e73f7758b
adicr7fan <adikshan11@gmail.com>
2023-12-09 03:32:21 +0530

    review

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,14 +1,14 @@
 // Reviews.jsx
 
-import React from 'react';
-import star from '../../assets/star.png';
-import emptystar from '../../assets/emptystar.png';
+import React from "react";
+import star from "../../assets/star.png";
+import emptystar from "../../assets/emptystar.png";
 
-const Reviews = () => {
+const Reviews = (rating) => {
   // Hardcoded array of reviews
   const reviews = [
-    { name: 'John Doe', rating: 4, comment: 'Great book!' },
-    { name: 'Jane Doe', rating: 5, comment: 'I loved it!' },
+    { name: "John Doe", rating: 4, comment: "Great book!" },
+    { name: "Jane Doe", rating: 5, comment: "I loved it!" },
     // Add more reviews as needed
   ];
 
@@ -20,14 +20,11 @@ const Reviews = () => {
         <div key={index} className="mb-4">
           <h3 className="text-lg font-bold">{review.name}</h3>
           <div className="flex items-center mb-1">
-            {[...Array(5)].map((_, i) => (
-              <img
-                key={i}
-                src={i < review.rating ? star : emptystar}
-                alt="star"
-                className="w-5 h-5 mr-1 cursor-pointer"
-              />
-            ))}
+                <div className="flex items-center">
+                  <img src={star} alt="" />
+                </div>
+              
+            
           </div>
           <p>{review.comment}</p>
         </div>
